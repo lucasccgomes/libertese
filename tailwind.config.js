f3bd6281ff3845793eaf77586adc95647a7ab1f9
Lucasccgomes <lucasccgomes@protonmail.com>
2023-11-20 22:09:09 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bghome': "url(https://lh3.googleusercontent.com/pw/ADCreHdWrtQvMBBS5DbinmMSZQ9nWky_F6lwi4LfQvoFpFsXfPEg07xEZZteNgo-YhBS1OH9OWKD_AQfSb1WInOy_7Nf83UYjCUDDphJO7FxbV0Wd7BsoNjt1KfsV_CVEF_TT8WjCtmDU4qcJ1hbYWkj2kAW-g=w703-h791-s-no-gm?authuser=0)",
+      }
+    },
+  },
+  plugins: [],
+}
